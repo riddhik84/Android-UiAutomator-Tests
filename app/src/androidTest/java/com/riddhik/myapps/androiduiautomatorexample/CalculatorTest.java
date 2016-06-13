820bd56bf8a20723a4846c052ada8ea899b77eda
riddhik84 <riddhik84@gmail.com>
2016-06-13 13:35:08 +0530

    First Commit

---
+++ app/src/androidTest/java/com/riddhik/myapps/androiduiautomatorexample/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.riddhik.myapps.androiduiautomatorexample;
+
+import org.junit.Before;
+
+/**
+ * Created by rkakadia on 5/26/2016.
+ */
+public class CalculatorTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    public void testAdd() throws Exception {
+
+    }
+}
